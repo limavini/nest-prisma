4bdffdad60d266330317a508cd5daa904c778f15
limavini <viniciusalmeidait@gmail.com>
2023-06-12 15:51:32 -0300

    add input validation

--- src/modules/book/dto/create-book.dto.ts
+++ src/modules/book/dto/create-book.dto.ts
@@ -1,12 +1,21 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
-  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @ApiPropertyOptional()
   description: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   bar_code: string;
 }
